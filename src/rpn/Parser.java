4cd20d62e87730bb522ce4a6f863e32e5d1a8a0b
artn <azemarteixeira@gmail.com>
2022-10-27 12:41:13 -0300

    Add hash map for variables

--- src/rpn/Parser.java
+++ src/rpn/Parser.java
@@ -1,13 +1,19 @@
 package rpn;
 
 import java.util.Stack;
+import java.util.HashMap;
 
 import tokens.Token;
 
 public class Parser {
-    Stack<Integer> s = new Stack<Integer>();
+    static Stack<Integer> s = new Stack<Integer>();
+    static HashMap<String, Integer> hm;
 
-    public Integer parse(Token token) {
+    static public void setVariables(HashMap<String, Integer> hm) {
+        this.hm = hm;
+    }
+
+    static public Integer parse(Token token) {
         switch (token.getType()) {
         case PLUS:
             return s.push(s.pop() + s.pop());
@@ -19,6 +25,11 @@ public class Parser {
             return s.push(s.pop() / s.pop());
         case NUM:
             s.push(Integer.parseInt(token.lexeme));
+        case IDENTIFIER: {
+            Integer value = s.push(hm.get(token.lexeme));
+            if (value == null)
+                throw new UnexpectedVariableException(token.lexeme);
+        }
 		default:
 			return 0;
         }
