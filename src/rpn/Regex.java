112f3c77cb07ef8a19ccd597cb353ea8aba91f2b
artn <azemarteixeira@gmail.com>
2022-10-27 12:39:47 -0300

    Add regex for variables

--- src/rpn/Regex.java
+++ src/rpn/Regex.java
@@ -3,8 +3,9 @@ package rpn;
 import tokens.TokenType;
 
 public class Regex {
-    static private final String PATTERN_OPERATION = "\\+|\\*|-|/";
+    static private final String PATTERN_OPERATION = "\\+|\\*|-|/|=";
     static private final String PATTERN_NUMBER    = "\\d+";
+    static private final String PATTERN_VARIABLE = "\\w[a-zA-Z0-9_]+|_+\\w[a-zA-Z0-9_]+";
 
     static public boolean isOp(String lexeme) {
     	return lexeme.matches(PATTERN_OPERATION);
@@ -12,6 +13,10 @@ public class Regex {
     
     static public boolean isNum(String lexeme) {
     	return lexeme.matches(PATTERN_NUMBER);
+    }
+
+    static public boolean isIdentifier(String lexeme) {
+        return lexeme.matches(PATTERN_VARIABLE);
     }
     
     static public TokenType getOpType(String lexeme) {    	
@@ -24,6 +29,8 @@ public class Regex {
             return TokenType.MULT;
         case "/":
             return TokenType.DIV;
+        case "=":
+            return TokenType.ASSIGN;
         default:
             return TokenType.NULL;
     	}
